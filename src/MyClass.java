38cfad66f64fda4a0ef72b3010b86504404f2807
AhmedMuhaameed <ahmed.m.abdelrahman0@gmail.com>
2018-11-27 16:16:05 +0200

    find min prime

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,10 +1,16 @@
-
+import java.util.Arrays;
 public class MyClass {
 
 	public MyClass() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	
+   	public static int find_min_prime(int arr[])
+   	 {
+      	 	 Arrays.sort(arr);
+       		 return arr[0];
+  	  }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

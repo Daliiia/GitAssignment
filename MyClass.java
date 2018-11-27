e3d627a1e743e8b7d80912cee72f5e9895671ee8
YousefKamel <45337708+YousefKamel@users.noreply.github.com>
2018-11-27 16:19:28 +0200

    Remove Char User Input Handling

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 import java.util.Scanner;
+///////////////////////////////////////
 public class MyClass {
+	public MyClass() 
+	{ }
+	/////////////////////////
         public static double getmedian(int []arr)
         {
         Arrays.sort(arr);
@@ -21,32 +25,10 @@ public class MyClass {
         }
         return median;
         }
-        public static char getmedian(char []arr)
-        {
-        Arrays.sort(arr);
-        char c;
-        if(arr.length%2==0) //even
-        {
-            int i=arr.length/2;
-            int k=(arr.length/2)-1;
-            int sum=(int)arr[k];
-            sum+=(int)arr[i];
-            sum/=2;
-            c=(char)sum;
-        }
-        else  //odd
-        {
-            int i=arr.length/2;
-            c=arr[i];
-        }
-        return c;
-        }
-	public MyClass() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        ///////////////////////////////////////////////////////////////
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
